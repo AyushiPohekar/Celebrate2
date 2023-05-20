23313d286b3e65e71ee5f92eb6e7ff356d7a0942
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-05-20 19:02:15 +0100

    crop btns done

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const imageUpload = document.getElementById("imageUpload");
 const customButton = document.getElementById("customButton");
 const previewContainer = document.getElementById("previewContainer");
-
+const btns=document.getElementById("btns")
+let cropper;
 
 imageUpload.addEventListener("change", function () {
-    // const fileName = this.value.split(/\\|\//).pop(); // Get the selected file name
-    // console.log(fileName)
-    // customButton.setAttribute('data-text', fileName);
-    // customButton.classList.add('input-selected');
+   
     const file = this.files[0];
   
     if (file) {
@@ -23,9 +21,16 @@ imageUpload.addEventListener("change", function () {
           previewImage.id = "previewImage";
           previewImage.src = this.src;
           previewContainer.innerHTML = "";
-          previewContainer.appendChild(previewImage);
+          previewContainer.append(previewImage,btns);
+          previewContainer.style.display="block"
   
-        
+          cropper = new Cropper(previewImage, {
+            aspectRatio: 1,
+            viewMode: 1,
+            dragMode: "move",
+            autoCropArea: 0.8,
+            zoomable:false
+          });
 
 
 
