fb07f8376ef3f215643d4f23fa5ada2555141187
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-05-20 20:44:49 +0100

    shape functionality remaining

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const customButton = document.getElementById("customButton");
 const previewContainer = document.getElementById("previewContainer");
 const btns=document.getElementById("btns");
 const cropButton = document.getElementById("cropButton");
-
+const modalContent=document.getElementById("modal-content")
 const rotateButton = document.getElementById("rotateButton");
 let cropper;
 
@@ -32,7 +32,7 @@ imageUpload.addEventListener("change", function () {
             viewMode: 1,
             dragMode: "move",
             autoCropArea: 0.8,
-            zoomable:false
+          
           });
 
 
@@ -54,11 +54,37 @@ imageUpload.addEventListener("change", function () {
   
     previewContainer.innerHTML = "";
     previewContainer.appendChild(croppedImage);
-  
+    //console.log(croppedImageData)
+    openModal(croppedImageData);
   
   });
   
   rotateButton.addEventListener("click", function () {
     cropper.rotate(90);
   });
+
+  function openModal(cropData) {
+    var modal = document.getElementById('modal');
+    var modalCanvas = document.getElementById('modal-canvas');
+  
+  
+  
+    
+  
+    const croppedImage2 = document.createElement("img");
+    croppedImage2.id = "croppedImage2";
+    croppedImage2.src = cropData;
+    modalCanvas.innerHTML="";
+    modalCanvas.appendChild(croppedImage2);
+    modalContent.appendChild(modalCanvas)
+    console.log(croppedImage2)
+    modal.style.display = 'block';
+    previewContainer.style.display="none"
+
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
   
